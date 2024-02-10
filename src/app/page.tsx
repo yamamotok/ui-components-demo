bf4eac575bc47f02409542de3ac2e777465e4aa6
Keisuke Yamamoto <2731953+yamamotok@users.noreply.github.com>
2024-02-11 08:12:06 +0900

    index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,16 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="container mx-auto py-8">
-      <Link href="/signal">Signal</Link>
-      <Link href="/codeinput">Product Code Input</Link>
-    </div>
+    <ul className="container mx-auto flex flex-col gap-8 py-8">
+      <li>
+        <Link href="/alphanumeric">Alpha-numeric inputs</Link>
+      </li>
+      <li>
+        <Link href="/signal">Signal</Link>
+      </li>
+      <li>
+        <Link href="/codeinput">Product Code Input</Link>
+      </li>
+    </ul>
   );
 }
