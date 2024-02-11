26070dfeb6e00e0e5cba1da9f7e11bbfc0ebbf3f
Keisuke Yamamoto <2731953+yamamotok@users.noreply.github.com>
2024-02-11 14:46:26 +0900

    Update index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <ul className="container mx-auto flex flex-col gap-8 py-8">
       <li>
+        <Link href="/validated">Generic ValidatedInput</Link>
+      </li>
+      <li>
         <Link href="/alphanumeric">Alpha-numeric inputs</Link>
       </li>
       <li>
